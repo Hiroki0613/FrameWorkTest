69c94559774822a07cb4b301ff3e5e00be11d5ad
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-11 22:51:47 +0900

    private init()を追加することで、イニシャル宣言を出来ないようにした

--- FrameWorkTest/RandomGenerator.swift
+++ FrameWorkTest/RandomGenerator.swift
@@ -11,10 +11,15 @@ import Foundation
 //コードを公開設定にする
 public class RandomGenerator {
     
+    // 宣言することで init()を使えなくする
+    private init() {}
+    
+    //　ランダムString
     static func string() -> String {
         return UUID().uuidString
     }
     
+    //ランダムInteger
     static func integer() -> Int {
         return Int(arc4random())
     }
