4312bfbeba9bc5a642dd65dcfa6e25a10dd6def8
Hiroki Kondo <kondo.matr02@gmail.com>
2020-03-11 23:00:12 +0900

    フレームワークを作成し、embed in

---
+++ FrameWorkTest/Random.swift
@@ -0,0 +1,26 @@
+//
+//  RandomGenerator.swift
+//  FrameWorkTest
+//
+//  Created by 近藤宏輝 on 2020/03/11.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+//import Foundation
+//
+////コードを公開設定にする
+//public class RandomGenerator {
+//    
+//    // 宣言することで init()を使えなくする
+//    private init() {}
+//    
+//    //　ランダムString
+//    static func string() -> String {
+//        return UUID().uuidString
+//    }
+//    
+//    //ランダムInteger
+//    static func integer() -> Int {
+//        return Int(arc4random())
+//    }
+//}
